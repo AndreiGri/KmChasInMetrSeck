ff6655b1cc7eff32b66725bac4e305d6a03821af
Gridin_Andrei <andreigridin918@gmail.com>
2023-08-05 21:27:24 +0500

    Добавьте файлы проекта.

---
+++ KmChasInMetrSeck/KmChasInMetrSeck.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+double KmChasInMetrSeck(int sp) {
+    double speedMetrInSeck = sp * 1000 / 60 / 60;
+    return speedMetrInSeck;
+}
+
+int main()
+{
+    system("chcp 1251 > nul");
+    double speed;
+    cout << "Введите скорость транспортного средства в километрах в час: ";
+    cin >> speed;
+    cout << "Скорость транспортного средства " << KmChasInMetrSeck(speed) << " метров в секунду!" << endl;
+    system("pause > nul");
+    return 0;
+}
